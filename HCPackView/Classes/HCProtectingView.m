c7e74ce7469cb9b60ef33e171da2dd85f96f018c
osh_1990@sina.cn <osh_1990@sina.cn>
2018-12-12 15:49:09 +0800

    Fixed

---
+++ HCPackView/Classes/HCProtectingView.m
@@ -0,0 +1,21 @@
+//
+//  HCProtectingView.m
+//  hellpCocoapods_Example
+//
+//  Created by Nathan Ou on 2018/12/12.
+//  Copyright © 2018 NathanOu. All rights reserved.
+//
+
+#import "HCProtectingView.h"
+
+@implementation HCProtectingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
